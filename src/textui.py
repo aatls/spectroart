856dd83f0242f75c90ce4d10a76d6db67dfdb4af
aatls <aatos.latva-somppi@helsinki.fi>
2025-11-06 16:35:19 +0200

    text based UI, first version

---
+++ src/textui.py
@@ -0,0 +1,69 @@
+import src.core as core
+
+class TextUi:
+    def __init__(self):
+        # Input parameters should be saved here
+        self.infile = None
+        self.outfile = None
+
+    def start(self):
+        greetings = """
+Hello! 
+Welcome to Spectroart, a nice program to make spectrogram art.
+Write 'help' if you want some help."""
+
+        print(greetings)
+
+        # Start UI loop
+        while True:
+            print()
+            cmd = input("Gimme instructions: ")
+            print()
+
+            match cmd:
+                case "help":
+                    self.print_help()
+                
+                case "load":
+                    self.infile = input("    Path to image: ")
+                    self.load_image()
+
+                case "convert":
+                    self.outfile = input("    Outfile name (Leave blank for [infile].wav): ")
+                    self.convert()
+
+                case "exit":
+                    self.exit_program()
+
+                case _:
+                    print(f"    '{cmd}' is not a valid command. Type 'help' for list of the commands")
+
+
+    def print_help(self):
+        help_msg = """    Instructions:
+
+    load:       Loads an image file.
+                Enter the full path to the image.
+                Working image formats not yet specified.
+            
+    convert:    Converts the image to audio.
+                Saves the result to user specified file or
+                [infile].wav if outfile left blank
+                
+    exit:       Exit the program."""
+
+        print(help_msg)
+
+    def load_image(self):
+        core.load_image(self.infile)
+
+    def convert(self):
+        if self.outfile == "":
+            # converts infile.ext to infile.wav
+            self.outfile = self.infile.split('.')[0] + ".wav"
+
+        core.convert(self.outfile)
+
+    def exit_program(self):
+        print("    See you later!")
+        exit()
