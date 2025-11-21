bba2b2f45f8e2d3eca93f39004242488134f2e46
aatls <aatos.latva-somppi@helsinki.fi>
2025-11-21 15:59:41 +0200

    refactoring 'generate_audio' with inner functions

--- src/imageprocessor.py
+++ src/imageprocessor.py
@@ -7,6 +7,7 @@ def flip_image(image, xy):
     
     
     ### Parameters
+    1. image : PIL Image object
     1. xy : str        
     """
     
@@ -20,64 +21,89 @@ def flip_image(image, xy):
     
     return image
 
-def generate_audio(pixel_data, samplerate, min_f, max_f):
-    """Generates audio from the saved image
+def generate_audio(image, samplerate, min_f, max_f):
+    """Generates audio from the input image
     
     ### Parameters
-    1. pixel_data : 2d array
+    1. pixel_data : PIL Image object
     2. samplerate : int
     3. min_f : float
     4. max_f : float
     """
 
-    pixel_data = np.array(pixel_data)
+    # Helper functions to make code more readable
+    def bw_conversion(pixel_data, weights=[1,1,1]):
+        # Apply weights to RGB values
+        pixel_data *= weights
+        # Pixel brightness = the mean of the RGB values
+        pixel_data = pixel_data.mean(axis=2)
 
-    width, height = np.shape(pixel_data)[:2]
+        return pixel_data
+
+    def normalize(data, old_range, new_range):
+        data *= new_range / old_range
+
+        return data
+
+    def add_power_contrast(data, power):
+        data **= power
+
+        return data
+
+    def generate_short_time_fourier_series(image, win_length, min_f, max_f):
+        width, height = np.shape(image)[:2]
+
+        n_positive_bins = height
+
+        # Map corresponding FFT bins for min_f and max_f
+        min_bin = int(round(min_f * win_length / samplerate))
+        max_bin = int(round(max_f * win_length / samplerate))
+
+        # Clip values within the set range
+        min_bin = np.clip(min_bin, 0, n_positive_bins - 1)
+        max_bin = np.clip(max_bin, 0, n_positive_bins - 1)
+
+
+        # Ensure all frequencies don't collapse into a single bin.
+
+        if min_bin == max_bin:
+            if max_bin < n_positive_bins - 1:
+                max_bin = min(min_bin + 1, n_positive_bins - 1)
+            else:
+                if min_bin > 0:
+                    min_bin = max(min_bin - 1, 0)
+
+
+        series = np.zeros((n_positive_bins, width), dtype=float)
+
+        # Linearly space target frequency bins between min_bin and max_bin
+        target_bins = np.linspace(min_bin, max_bin, num=height)
+        target_bins_int = np.round(target_bins).astype(int) # round to nearest integer
+
+        for y in range(height):
+            k = int(target_bins_int[y]) # target frequency bin for the row_values
+            series[k, :] = image[:, y] # assign values of row_values to row to target frequency bin k
+
+        return series
+
+    # Convert the pixel data into a numpy array with type 'float'
+    image = np.array(image).astype(float)
+
+    # Modify the pixel data
+    image = bw_conversion(image, weights=[0.2126, 0.7152, 0.0722])
+    image = normalize(image, 255, 1)
+    image = add_power_contrast(image, 2)
+
+    width, height = np.shape(image)[:2]
 
     # Window length is 2 x image height because
     # bin[x] = bin[-x] when x < image height
     win_length = 2 * height - 1
 
-    n_positive_bins = height
-
-    # Map corresponding FFT bins for min_f and max_f
-    min_bin = int(round(min_f * win_length / samplerate))
-    max_bin = int(round(max_f * win_length / samplerate))
-
-    # Clip values within the set range
-    min_bin = np.clip(min_bin, 0, n_positive_bins - 1)
-    max_bin = np.clip(max_bin, 0, n_positive_bins - 1)
-
-
-    # Ensure all frequencies don't collapse into a single bin.
-
-    if min_bin == max_bin:
-        if max_bin < n_positive_bins - 1:
-            max_bin = min(min_bin + 1, n_positive_bins - 1)
-        else:
-            if min_bin > 0:
-                min_bin = max(min_bin - 1, 0)
-
-
-    series = np.zeros((n_positive_bins, width), dtype=float)
-
-    # Linearly space target frequency bins between min_bin and max_bin
-    target_bins = np.linspace(min_bin, max_bin, num=height)
-    target_bins_int = np.round(target_bins).astype(int) # round to nearest integer
-
-    for y in range(height):
-        row_values = [] # buffer for brightness values of the image row y
-        for x in range(width):
-            normalized = pixel_data[x, height - 1 - y] / 255
-            normalized *= [0.2126, 0.7152, 0.0722]
-            total = np.sum(normalized)
-            brightness = total ** 2
-            row_values.append(brightness)
-        k = int(target_bins_int[y]) # target frequency bin for the row_values
-        series[k, :] = row_values # assign values of row_values to row to target frequency bin k
+    series = generate_short_time_fourier_series(image, win_length, min_f, max_f)
 
     SFT = ShortTimeFFT(win=np.hanning(win_length),
-                    hop=(height),
+                    hop=(win_length // 2),
                     fs=samplerate)
 
     # Create audio with inverse STFT
